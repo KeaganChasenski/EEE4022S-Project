185ddb69735f85e9bdd4a676a0cb7571e83d4b34
KeaganChasenski <k.chasenski@gmail.com>
2021-10-29 17:24:51 +0200

    Added test_df.py file to get test results of phase from FM signals for results section of the report.

---
+++ Python/test_df.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import time 
+import numpy as np
+import store_IQ
+
+
+#while True:
+    #print("Reading FM data to files")
+store_IQ.main()
+    #print("Saved FM data to files")
+    
+    #time.sleep(3)
+    
+   
+    
+    
+    
